a00feb2ea7f126099700ee5b922514f9318d65d9
Anton Minin <vedmak17@gmail.com>
2016-02-05 21:45:52 +0300

    some changes

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,28 @@ var router = express.Router();
 var passport = require('passport');
 
 router.get('/', function(req, res) {
-  res.render('admin', { user : req.user });
+  res.render('admin-login', { user : req.user });
 });
 
-router.post('/', passport.authenticate('local', { failureRedirect: '/' }), function(req, res) {
+router.post('/', passport.authenticate('local', { failureRedirect: '/admin/error'}), function(req, res) {
   res.redirect('/admin-index');
 });
 
+router.get('/error', function(req, res) {
+  res.render('admin-error', {title : "equilibrium"});
+});
+
+//need authorization verify
 router.get('/index', function(req, res) {
+  //if (!req.user) {
+    //res.redirect('/admin/error');
+  //};
   res.render('admin-index', { user : req.user });
+});
+
+//need authorization verify
+router.get('/accounts', function(req, res) {
+  res.render('admin-accounts', { user : req.user });
 });
 
 module.exports = router;
